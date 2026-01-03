778cbd522f5ef5c78b3b91d74fd8cfdd114287d3
Yara-dgh <yaradoghaim84@gmail.com>
2026-01-03 12:35:47 +0300

    Add files via upload

---
+++ lab1.dart
@@ -0,0 +1,7 @@
+void main() {
+  var string = "jordan,peterson";
+  print(string.split(','));
+  String FirstName = "AHMAD ISMAIL ALI";
+  print(FirstName.substring(0, 5));
+  print(FirstName.substring(0, 1).toUpperCase() + FirstName.substring(1));
+}
